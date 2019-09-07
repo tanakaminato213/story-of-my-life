0d060f814a9defb632ea662372ce407b7f356965
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-07 23:04:35 +0900

    グループ編集機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
   def index
   end
 
@@ -15,6 +16,7 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
