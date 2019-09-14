d90030d8c81bde8925243ad307b28104f4fce42b
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-14 16:15:04 +0900

    カラムとサイドバーの修正

--- db/migrate/20190904100041_remove_author_from_groups.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAuthorFromGroups < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :groups, :user_id
-      end
-
-  def down
-    add_column :groups, :user, foreign_key: true
-  end
-end
